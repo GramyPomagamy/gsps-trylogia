01970990288d4bff4078f1b912869c3c9376f6d2
hoXyy <hoxtf2@outlook.com>
2021-07-15 17:01:59 +0200

    Updates

---
+++ src/dashboard/completion.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { useReplicant } from 'use-nodecg';
+import { Container, createTheme, ThemeProvider, Typography } from '@material-ui/core';
+
+
+
+const Completion = () => {
+    const darkTheme = createTheme({
+        palette: {
+            type: 'dark'
+        }
+    })
+
+    const [completion] = useReplicant('completion');
+    const [currentSplit] = useReplicant('currentSplit');
+
+    return(
+        <ThemeProvider theme={darkTheme}>
+            <Container>
+                <Typography align="center" variant="h6">Obecny procent ukończenia {currentSplit}:</Typography>
+                <Typography align="center" variant="h4"><b>{completion}%</b></Typography>
+        </ThemeProvider>
+    )
+}
+
+render(<Completion/>, document.getElementById('root'));
