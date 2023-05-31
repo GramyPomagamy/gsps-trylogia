66e0076a306d1ee6408ced4a28b38abb226318a2
hoXyy <herganus@gmail.com>
2023-05-31 03:10:38 +0200

    Start adding main layout

---
+++ src/graphics/gra/main.vue
@@ -0,0 +1,26 @@
+<template><div id="container"></div></template>
+
+<script setup lang="ts">
+import { useReplicant } from "nodecg-vue-composable";
+import { useHead } from "@vueuse/head";
+import { CurrentSplit, Splits, Timer } from "@gsps-trylogia/types/schemas";
+
+useHead({ title: "Główny layout" });
+
+const timer = useReplicant<Timer>("timer", "gsps-trylogia");
+const currentSplit = useReplicant<CurrentSplit>(
+  "currentSplit",
+  "gsps-trylogia"
+);
+const splits = useReplicant<Splits>("splits", "gsps-trylogia");
+</script>
+
+<style>
+#container {
+  background-image: url("./background.png");
+  width: 1920px;
+  height: 1030px;
+  margin: 0;
+  padding: 0;
+}
+</style>
